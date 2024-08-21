2af5977e8da00fd1f13767787bc9e03c4a77108f
JuliaJakubcova <jakubcovajulia@gmail.com>
2024-08-21 13:49:41 +0200

    [KMM, AN] Remove unused project constants, add configChanges to manifest

--- build-logic/convention/src/main/kotlin/constants/ProjectConstants.kt
+++ build-logic/convention/src/main/kotlin/constants/ProjectConstants.kt
@@ -3,16 +3,6 @@ package constants
 object ProjectConstants {
     const val iosShared = "KMPShared"
 
-    object Android {
-        private const val root = ":android"
-        const val shared = "$root:shared"
-        const val login = "$root:login"
-        const val profile = "$root:profile"
-        const val users = "$root:users"
-        const val recipes = "$root:recipes"
-        const val books = "$root:books"
-    }
-
     object Variant {
         const val debug = "debug"
         const val alpha = "alpha"
