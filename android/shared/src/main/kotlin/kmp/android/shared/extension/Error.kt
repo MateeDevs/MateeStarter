2dcbad6dcef4dffdbb7a4c47c73f2eb05987b3c5
JuliaJakubcova <jakubcovajulia@gmail.com>
2024-09-24 16:09:31 +0200

    [KMM] Add StringDesc to ErrorResult, remove separate error mapping

--- android/shared/src/main/kotlin/kmp/android/shared/extension/Error.kt
+++ android/shared/src/main/kotlin/kmp/android/shared/extension/Error.kt
@@ -4,20 +4,18 @@ import android.annotation.SuppressLint
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import kmp.shared.base.ErrorResult
-import kmp.shared.base.error.ErrorMessageProvider
-import kmp.shared.base.error.getMessage
 import kotlinx.coroutines.flow.Flow
-import org.koin.androidx.compose.get
 
 @SuppressLint("ComposableNaming")
 @Suppress("konsist.every internal or public compose function has a modifier")
 @Composable
 infix fun Flow<ErrorResult>.showIn(snackHost: SnackbarHostState) {
-    val errorMessageProvider: ErrorMessageProvider = get()
+    val context = LocalContext.current
     LaunchedEffect(this, snackHost) {
         collect { error ->
-            snackHost.showSnackbar(errorMessageProvider.getMessage(error))
+            snackHost.showSnackbar(error.localizedMessage.toString(context))
         }
     }
 }
--- shared/base/src/commonMain/kotlin/kmp/shared/base/error/domain/CommonError.kt
+++ shared/base/src/commonMain/kotlin/kmp/shared/base/error/domain/CommonError.kt
@@ -1,12 +1,18 @@
 package kmp.shared.base.error.domain
 
+import dev.icerock.moko.resources.desc.StringDesc
+import dev.icerock.moko.resources.desc.desc
 import kmp.shared.base.ErrorResult
+import kmp.shared.base.MR
 
 /**
  * Error type used anywhere in the project. Contains subclasses for common exceptions that can happen anywhere
  * @param throwable optional [Throwable] parameter used for debugging or crash reporting
  */
-sealed class CommonError(throwable: Throwable? = null) : ErrorResult(throwable = throwable) {
-    class NoNetworkConnection(t: Throwable?) : CommonError(t)
-    data object Unknown : CommonError()
+sealed class CommonError(localizedMessage: StringDesc, throwable: Throwable? = null) : ErrorResult(
+    localizedMessage = localizedMessage,
+    throwable = throwable,
+) {
+    class NoNetworkConnection(throwable: Throwable?) : CommonError(localizedMessage = MR.strings.error_no_internet_connection.desc(), throwable = throwable)
+    data object Unknown : CommonError(localizedMessage = MR.strings.unknown_error.desc())
 }
