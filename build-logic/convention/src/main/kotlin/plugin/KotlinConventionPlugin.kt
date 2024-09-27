b6c05ad32a0464e2931cd7b0ff6520752d304b01
Daniel Zurek <daniel.zurek@matee.cz>
2024-09-27 13:23:05 +0200

    ♻️refactor deprecated kotlinOptions

--- build-logic/convention/src/main/kotlin/plugin/KotlinConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/plugin/KotlinConventionPlugin.kt
@@ -1,15 +1,11 @@
 package plugin
 
-import extensions.apply
-import extensions.implementation
-import extensions.kotlin
-import extensions.libs
-import extensions.pluginManager
-import org.gradle.api.JavaVersion
+import extensions.*
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 @Suppress("unused")
@@ -21,17 +17,16 @@ class KotlinConventionPlugin : Plugin<Project> {
                 apply(libs.plugins.kotlin.android)
             }
 
-            val versionCode = libs.versions.java.get().toInt()
-            val javaVersion = JavaVersion.toVersion(versionCode)
+            val javaVersionCode = libs.versions.java.get().toInt()
 
             kotlin {
-                jvmToolchain(versionCode)
+                jvmToolchain(javaVersionCode)
             }
 
             tasks.withType<KotlinCompile> {
-                kotlinOptions {
-                    jvmTarget = javaVersion.toString()
-                    freeCompilerArgs = freeCompilerArgs + listOf(
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.fromTarget(javaVersionCode.toString()))
+                    freeCompilerArgs.addAll(
                         "-Xallow-jvm-ir-dependencies",
                         "-opt-in=kotlin.RequiresOptIn",
                     )
