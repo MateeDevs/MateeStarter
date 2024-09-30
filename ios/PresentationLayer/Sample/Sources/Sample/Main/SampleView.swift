48f2aee9f707cd10f0059ab3af7f941824a20c74
Lukáš Matuška <47986278+MatuskaDev@users.noreply.github.com>
2024-09-30 17:58:21 +0200

    SwiftUI ViewData (#6)
    * ✨ [iOS] Add ViewData for native SwiftUI views with native VM
    
    * Update SampleView
    
    Co-authored-by: Tomáš Batěk <64600422+tomas-bat@users.noreply.github.com>
    
    * PR fixes
    
    ---------
    
    Co-authored-by: Tomáš Batěk <64600422+tomas-bat@users.noreply.github.com>

--- ios/PresentationLayer/Sample/Sources/Sample/Main/SampleView.swift
+++ ios/PresentationLayer/Sample/Sources/Sample/Main/SampleView.swift
@@ -5,6 +5,7 @@
 
 import SwiftUI
 import UIToolkit
+import KMPShared
 
 struct SampleView: View {
     
@@ -15,19 +16,17 @@ struct SampleView: View {
     }
     
     var body: some View {
-        ZStack(alignment: .center) {
-            if viewModel.state.isLoading {
-                PrimaryProgressView()
-            } else {
-                VStack(spacing: AppTheme.Dimens.spaceMedium) {
-                    Text("This is a sample with SwiftUI and iOS VM")
-                    
-                    Text(viewModel.state.sampleText?.value ?? "")
-                    
-                    Button("Click me!") {
-                        viewModel.onIntent(.onButtonTapped)
-                    }
+        Group {
+            switch viewModel.state.sampleText {
+            case let .data(text), let .loading(text):
+                contentView(sampleText: text) {
+                    viewModel.onIntent(.onButtonTapped)
                 }
+                .skeleton(viewModel.state.sampleText.isLoading)
+            case let .error(error):
+                ErrorView(error: error)
+            case .empty:
+                EmptyView()
             }
         }
         .navigationTitle(L10n.bottom_bar_item_1)
@@ -36,6 +35,19 @@ struct SampleView: View {
             set: { toast in viewModel.onIntent(.onToastChanged(data: toast)) }
         ))
         .lifecycle(viewModel)
+    }
+    
+    private func contentView(
+        sampleText: SampleText,
+        onButtonTapped: @escaping () -> Void
+    ) -> some View {
+        VStack(spacing: AppTheme.Dimens.spaceMedium) {
+            Text("This is a sample with SwiftUI and iOS VM")
+            
+            Text(sampleText.value)
+            
+            Button("Click me!", action: onButtonTapped)
+        }
     }
 }
 
