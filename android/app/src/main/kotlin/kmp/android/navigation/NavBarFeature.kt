12bd5ff0e433e16c28c5d930fc5db100bdab06ee
Lukáš Matuška <lukas.matuska@icloud.com>
2024-10-10 12:43:21 +0200

    ✨ Use Moko in Android

--- android/app/src/main/kotlin/kmp/android/navigation/NavBarFeature.kt
+++ android/app/src/main/kotlin/kmp/android/navigation/NavBarFeature.kt
@@ -4,10 +4,10 @@ import androidx.annotation.StringRes
 import kmp.android.sample.navigation.SampleGraph
 import kmp.android.samplecomposemultiplatform.navigation.SampleComposeMultiplatformGraph
 import kmp.android.samplesharedviewmodel.navigation.SampleSharedViewModelGraph
-import kmp.android.shared.R
+import kmp.shared.base.MR
 
 enum class NavBarFeature(val route: String, @StringRes val titleRes: Int) {
-    Sample(SampleGraph.rootPath, R.string.bottom_bar_item_1),
-    SampleSharedViewModel(SampleSharedViewModelGraph.rootPath, R.string.bottom_bar_item_2),
-    SampleComposeMultiplatform(SampleComposeMultiplatformGraph.rootPath, R.string.bottom_bar_item_3),
+    Sample(SampleGraph.rootPath, MR.strings.bottom_bar_item_1.resourceId),
+    SampleSharedViewModel(SampleSharedViewModelGraph.rootPath, MR.strings.bottom_bar_item_2.resourceId),
+    SampleComposeMultiplatform(SampleComposeMultiplatformGraph.rootPath, MR.strings.bottom_bar_item_3.resourceId),
 }
