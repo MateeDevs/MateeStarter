900f6d1c0054f5eeade3b9e54ca93e43679fd20b
Lukáš Matuška <lukas.matuska@icloud.com>
2024-10-12 17:41:41 +0200

    ✨ [AND]  Change titleRes in NavBarFeature to StringDesc

--- android/app/src/main/kotlin/kmp/android/navigation/NavBarFeature.kt
+++ android/app/src/main/kotlin/kmp/android/navigation/NavBarFeature.kt
@@ -1,13 +1,14 @@
 package kmp.android.navigation
 
-import androidx.annotation.StringRes
+import dev.icerock.moko.resources.desc.StringDesc
+import dev.icerock.moko.resources.desc.desc
 import kmp.android.sample.navigation.SampleGraph
 import kmp.android.samplecomposemultiplatform.navigation.SampleComposeMultiplatformGraph
 import kmp.android.samplesharedviewmodel.navigation.SampleSharedViewModelGraph
 import kmp.shared.base.MR
 
-enum class NavBarFeature(val route: String, @StringRes val titleRes: Int) {
-    Sample(SampleGraph.rootPath, MR.strings.bottom_bar_item_1.resourceId),
-    SampleSharedViewModel(SampleSharedViewModelGraph.rootPath, MR.strings.bottom_bar_item_2.resourceId),
-    SampleComposeMultiplatform(SampleComposeMultiplatformGraph.rootPath, MR.strings.bottom_bar_item_3.resourceId),
+enum class NavBarFeature(val route: String, val titleRes: StringDesc) {
+    Sample(SampleGraph.rootPath, MR.strings.bottom_bar_item_1.desc()),
+    SampleSharedViewModel(SampleSharedViewModelGraph.rootPath, MR.strings.bottom_bar_item_2.desc()),
+    SampleComposeMultiplatform(SampleComposeMultiplatformGraph.rootPath, MR.strings.bottom_bar_item_3.desc()),
 }
