08c92c7ad09e605ca3034c04d1738645b6c3c8dd
Lukáš Matuška <lukas.matuska@icloud.com>
2024-10-17 16:41:01 +0200

    🧹 [AND] Cleanup

--- shared/base/src/androidMain/kotlin/kmp/shared/base/di/AndroidModule.kt
+++ shared/base/src/androidMain/kotlin/kmp/shared/base/di/AndroidModule.kt
@@ -5,8 +5,8 @@ import kmp.shared.analytics.data.source.AnalyticsSource
 import kmp.shared.base.analytics.AndroidAnalyticsSourceImpl
 import kmp.shared.base.system.Config
 import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.module
 import org.koin.dsl.bind
+import org.koin.dsl.module
 
 internal actual val platformModule = module {
     singleOf(::Config)
