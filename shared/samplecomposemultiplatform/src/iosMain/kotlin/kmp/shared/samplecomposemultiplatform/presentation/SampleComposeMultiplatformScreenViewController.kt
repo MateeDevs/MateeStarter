373f8c63d5636e225bb1168895ab6eb1b61a7e41
JuliaJakubcova <102815339+JuliaJakubcova@users.noreply.github.com>
2025-01-10 11:41:34 +0100

    [KMM, iOS] SwiftUI view inside compose view demonstration (#32)
    * [KMM] Add PlatformSpecificCheckboxView to demonstrate usage of compose-swift-bridge plugin
    
    * [KMM] Fix build.gradle configuration, fix lint issues
    
    * [iOS] Remove Binding class, use checkbox view directly in the factory
    
    * [KMM] Add all classes manually to avoid using compose-swift-bridge plugin
    
    * [KMM] Remove unused dependencies, add comment about plugin, suppress konsist issues
    
    * [iOS] Fix after rebase
    
    * [KMM] Fix konsist issues
    
    * [AN] Fix test

--- shared/samplecomposemultiplatform/src/iosMain/kotlin/kmp/shared/samplecomposemultiplatform/presentation/SampleComposeMultiplatformScreenViewController.kt
+++ shared/samplecomposemultiplatform/src/iosMain/kotlin/kmp/shared/samplecomposemultiplatform/presentation/SampleComposeMultiplatformScreenViewController.kt
@@ -1,12 +1,16 @@
 package kmp.shared.samplecomposemultiplatform.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.ComposeUIViewController
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kmp.shared.samplecomposemultiplatform.presentation.common.AppTheme
+import kmp.shared.samplecomposemultiplatform.presentation.ui.LocalSampleComposeMultiplatformViewFactory
 import kmp.shared.samplecomposemultiplatform.presentation.ui.SampleComposeMultiplatformScreen
+import kmp.shared.samplecomposemultiplatform.presentation.ui.SampleComposeMultiplatformViewFactory
 import kmp.shared.samplesharedviewmodel.vm.SampleSharedEvent
 import kmp.shared.samplesharedviewmodel.vm.SampleSharedIntent
 import kmp.shared.samplesharedviewmodel.vm.SampleSharedViewModel
@@ -17,15 +21,21 @@ import platform.UIKit.UIViewController
 @Suppress("Unused", "FunctionName")
 fun SampleComposeMultiplatformScreenViewController(
     onEvent: (SampleSharedEvent) -> Unit,
+    factory: SampleComposeMultiplatformViewFactory,
 ): UIViewController {
     return ComposeUIViewController {
-        SampleComposeMultiplatformView(onEvent = onEvent)
+        SampleComposeMultiplatformView(
+            onEvent = onEvent,
+            factory = factory,
+        )
     }
 }
 
 @Composable
 internal fun SampleComposeMultiplatformView(
     onEvent: (SampleSharedEvent) -> Unit,
+    factory: SampleComposeMultiplatformViewFactory,
+    modifier: Modifier = Modifier,
 ) {
     val viewModel: SampleSharedViewModel = koinViewModel()
     val state by viewModel.state.collectAsStateWithLifecycle()
@@ -39,8 +49,15 @@ internal fun SampleComposeMultiplatformView(
             onEvent(event)
         }
     }
-
-    AppTheme {
-        SampleComposeMultiplatformScreen(state = state, onIntent = viewModel::onIntent)
+    CompositionLocalProvider(
+        LocalSampleComposeMultiplatformViewFactory provides factory,
+    ) {
+        AppTheme {
+            SampleComposeMultiplatformScreen(
+                state = state,
+                onIntent = viewModel::onIntent,
+                modifier = modifier,
+            )
+        }
     }
 }
