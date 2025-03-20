69ababf20cbac0edf43a71b6790035701e326c46
Lukáš Matuška <lukas.matuska@icloud.com>
2025-03-20 14:03:19 +0100

    ✨ [iOS] Add Firebase Analytics implementation

--- ios/DataLayer/Providers/AnalyticsProvider/Package.swift
+++ ios/DataLayer/Providers/AnalyticsProvider/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "SharedDomain", path: "../../../DomainLayer/SharedDomain"),
-        .package(name: "Utilities", path: "../../../DomainLayer/Utilities")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "10.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +26,9 @@ let package = Package(
             name: "AnalyticsProvider",
             dependencies: [
                 .product(name: "SharedDomain", package: "SharedDomain"),
-                .product(name: "Utilities", package: "Utilities")
+                .product(name: "FirebaseAnalyticsWithoutAdIdSupport", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCore", package: "firebase-ios-sdk")
             ]
         )
     ]
--- ios/DataLayer/Providers/AnalyticsProvider/Sources/AnalyticsProvider/IosAnalyticsProviderImpl.swift
+++ ios/DataLayer/Providers/AnalyticsProvider/Sources/AnalyticsProvider/IosAnalyticsProviderImpl.swift
@@ -3,19 +3,23 @@
 //  Copyright © 2021 Matee. All rights reserved.
 //
 
+import Firebase
+import FirebaseAnalytics
 import KMPShared
-import OSLog
-import Utilities
 
 /**
  * iOS implementation of the AnalyticsSource from shared module
  */
 public class IosAnalyticsProviderImpl: AnalyticsProvider {
     
-    public init() { }
+    public init() {
+        // Start Firebase if not yet started
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+    }
     
     public func logEvent(event: AnalyticsEvent) {
-        // TODO: Implement analytics event logging
-        Logger.app.error("Analytics event logging not implemented")
+        Analytics.logEvent(event.eventName, parameters: event.parameters)
     }
 }
