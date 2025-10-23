71d33a2eb0c2489facabb7e49ae83b539b0c2deb
Tomáš Batěk <tomas.bat@icloud.com>
2025-10-23 22:29:22 +0100

    ✨ Migrate to scene phase

--- ios/Application/MateeStarterApp.swift
+++ ios/Application/MateeStarterApp.swift
@@ -4,15 +4,25 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 @main
 struct MateeStarterApp: App {
+    
+    @Environment(\.scenePhase) private var scenePhase
     
     @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     
     var body: some Scene {
         WindowGroup {
             AppRootView()
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .background:
+                WidgetCenter.shared.reloadAllTimelines()
+            default: break
+            }
         }
     }
 }
